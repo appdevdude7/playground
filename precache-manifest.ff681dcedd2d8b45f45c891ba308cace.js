2b411e3bd633b1603ff94d7620fb59485f6889e9
appdevdude7 <appdeveloper.akshay@gmail.com>
2019-12-22 14:42:39 -0600

    Updates

---
+++ precache-manifest.ff681dcedd2d8b45f45c891ba308cace.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4542cff7be4f4153077f8791f89af14",
+    "url": "/fun-app/index.html"
+  },
+  {
+    "revision": "94124c7dfac971953442",
+    "url": "/fun-app/static/css/main.84f7185d.chunk.css"
+  },
+  {
+    "revision": "916410d701e9866baca4",
+    "url": "/fun-app/static/js/2.67f42fef.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/fun-app/static/js/2.67f42fef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94124c7dfac971953442",
+    "url": "/fun-app/static/js/main.9a4c9bc6.chunk.js"
+  },
+  {
+    "revision": "9d65295d5ec2fd34f3b2",
+    "url": "/fun-app/static/js/runtime-main.49709311.js"
+  }
+]);
